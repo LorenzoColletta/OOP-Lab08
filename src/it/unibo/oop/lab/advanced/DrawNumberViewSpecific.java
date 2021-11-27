b3093fd23a9e6b1ce85a375db2eaa6a254de98e5
Lorenzo Colletta <id.boh06@gmail.com>
2021-11-27 22:52:08 +0100

    Add new DrwaNumberView implementation

---
+++ src/it/unibo/oop/lab/advanced/DrawNumberViewSpecific.java
@@ -0,0 +1,45 @@
+package it.unibo.oop.lab.advanced;
+
+public class DrawNumberViewSpecific implements DrawNumberView {
+
+    public DrawNumberViewSpecific() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void setObserver(DrawNumberViewObserver observer) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void numberIncorrect() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void result(DrawResult res) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void limitsReached() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void displayError(String message) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
