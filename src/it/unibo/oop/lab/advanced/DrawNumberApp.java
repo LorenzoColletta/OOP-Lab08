755f820e34ee098631d9b280ebbd7d69f8d78cba
Lorenzo Colletta <id.boh06@gmail.com>
2021-11-27 21:43:05 +0100

    Add UI to show errors, make settings load from file

--- src/it/unibo/oop/lab/advanced/DrawNumberApp.java
+++ src/it/unibo/oop/lab/advanced/DrawNumberApp.java
@@ -1,23 +1,50 @@
 package it.unibo.oop.lab.advanced;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  */
 public final class DrawNumberApp implements DrawNumberViewObserver {
 
-    private static final int MIN = 0;
-    private static final int MAX = 100;
-    private static final int ATTEMPTS = 10;
+//    private static final int MIN = 0;
+//    private static final int MAX = 100;
+//    private static final int ATTEMPTS = 10;
+//    private final Configuration config;
     private final DrawNumber model;
     private final DrawNumberView view;
 
     /**
-     * 
+     * @param fileConfig
+     *      file to retrieve configuration from
      */
-    public DrawNumberApp() {
-        this.model = new DrawNumberImpl(MIN, MAX, ATTEMPTS);
+    public DrawNumberApp(final String fileConfig) {
         this.view = new DrawNumberViewImpl();
         this.view.setObserver(this);
         this.view.start();
+        var min = 0;
+        var max = 0;
+        var attempts = 0;
+        try (BufferedReader configReader = new BufferedReader(new InputStreamReader(new FileInputStream(fileConfig)))) {
+            for (String line = configReader.readLine(); line != null; line = configReader.readLine()) {
+                if (line.contains("max")) {
+                    max = Integer.parseInt(line.replaceFirst("max: ", ""));
+                } else if (line.contains("attempts")) {
+                    attempts = Integer.parseInt(line.replaceFirst("attempts: ", ""));
+                } else if (line.contains("min")) {
+                    min = Integer.parseInt(line.replaceFirst("min: ", ""));
+                } else {
+                    this.view.displayError("Wrong file format.");
+                }
+            }
+        } catch (IOException e1) {
+            this.view.displayError("IO Error: " + e1.getMessage());
+        }
+        this.model = new DrawNumberImpl(min, max, attempts);
+
     }
 
     @Override
@@ -47,7 +74,7 @@ public final class DrawNumberApp implements DrawNumberViewObserver {
      *            ignored
      */
     public static void main(final String... args) {
-        new DrawNumberApp();
+        new DrawNumberApp("config.yml");
     }
 
 }
